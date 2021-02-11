e0ed89ccf4323ab8977bab3e00aa12da6399ce94
Tamas Csala <icyplusplus@gmail.com>
2021-02-11 19:42:46 +0000

    Hello osztalynevsor

--- ZoomBreakoutRoomAllocator/Program.cs
+++ ZoomBreakoutRoomAllocator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace ZoomBreakoutRoomAllocator
 {
@@ -6,7 +8,36 @@ namespace ZoomBreakoutRoomAllocator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                throw new Exception($"Usage: {nameof(ZoomBreakoutRoomAllocator)}.exe path/to/participantsName.txt");
+            }
+
+            Random rnd = new Random();
+            var participants = File.ReadLines(args[0]).Where(x => x.Length != 0).OrderBy(x => rnd.Next()).ToArray();
+
+            Console.WriteLine("Please enter the number of breakout rooms to allocate");
+            var roomCountStr = Console.ReadLine();
+            int roomCount;
+
+            while (!int.TryParse(roomCountStr, out roomCount))
+            {
+                Console.WriteLine($"{roomCountStr} is not an integer, please enter a number");
+                roomCountStr = Console.ReadLine();
+            }
+
+
+
+            int p = 0;
+            for (int i = 0; i < roomCount; ++i)
+            {
+                Console.WriteLine($"Room {i + 1}:");
+                for (; p < (i+1)*(participants.Length / roomCount); ++p)
+                {
+                    Console.WriteLine(participants[p]);
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
